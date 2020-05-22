84da6c19d56735aca4a96ea7e9fec30d1e4dec70
Pablo Matías Gomez <pablomatiasgomez@gmail.com>
2020-05-22 19:20:38 -0300

    Update enc_passowrd-generator.js

--- enc_passowrd-generator.js
+++ enc_passowrd-generator.js
@@ -3,25 +3,9 @@ const webcrypto = new WebCrypto();
 const tweetnacl = require("tweetnacl");
 tweetnacl.sealedbox = require('tweetnacl-sealedbox-js');
 
-exports.handler = async (event) => {
-  const APP_ID = '6';
-  const KEY_ID = '191';
-  const PUBLIC_KEY = '47c741b21d867bf5909ec70f86da86b9adcdff119c365f44f19d2f222f60260b';
-  const PASSWORD = 'passwrod123';
+exports.encrypt = async (appId, keyId, publicKey, password) => {
   const time = Math.floor(Date.now() / 1000).toString();
-  let result;
-
-  try {
-    result = await encryptPassword(APP_ID, KEY_ID, PUBLIC_KEY, PASSWORD, time);
-    console.log(result);
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify(result),
-    };
-    return response;
-  } catch (e) {
-    console.log(e)
-  }
+  return await encryptPassword(appId, keyId, publicKey, password, time);
 };
 
 async function encryptPassword(app_id, key_id, public_key, password, time) {
